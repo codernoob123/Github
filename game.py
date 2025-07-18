e03a6c7c0062028f1a508182a70fc4cc43aa12cb
StarDJ-VNS <100501406+codernoob123@users.noreply.github.com>
2025-07-18 14:29:16 +0700

    version 1.1

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
@@ -7,14 +8,6 @@ class Background(pygame.sprite.Sprite):
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-
-def isTouched(mouse_x, mouse_y, border_x, border_y, width, length):
-    if (mouse_x >= border_x and mouse_x <= border_x + width) and (mouse_y >= border_y and mouse_y <= border_y + length):
-        return True
-    else:
-        return False 
-    
-
 
 pygame.init()
 
@@ -25,12 +18,20 @@ screen = pygame.display.set_mode((width,length))
 pygame.display.set_caption("Clicking Game")
 
 background = Background('menu.jpg', [0,0])
+background2 = Background('menu2.jpg', [0,0])
+ball = pygame.image.load('ball.png')
 
 running = True
-drawings = True
+not_starting = True
+Isitend = False
+
 score = 0
 cir_x = 0
 cir_y = 0
+start_time = 0
+second = 0
+game_time = 20
+
 try:
     with open("highscore.txt", "r") as file:
         high_score = int(file.read())
@@ -40,57 +41,107 @@ except:
     with open("highscore.txt", "w") as file:
         file.write(str(score))
 
+font = pygame.font.Font('freesansbold.ttf', 18)
+menufont = pygame.font.Font('freesansbold.ttf', 40)
+namefont = pygame.font.Font('freesansbold.ttf', 60)
+endfont = pygame.font.Font('freesansbold.ttf', 50)
+endfont_2 = pygame.font.Font('freesansbold.ttf',30)
+
+name_text = namefont.render('Clicking Game', True, (255,255,255), (0,0,0))
+name_rect = name_text.get_rect()
+name_rect.center = (400,100)
+
+gameover_text = endfont.render('GAME OVER', True, (255,255,255), (0,0,0))
+gameover_rect = gameover_text.get_rect()
+gameover_rect.center = (400,150)
+
+start_text = menufont.render('START', True, (255,255,255), (0,0,0))
+startRect = start_text.get_rect()
+startRect.center =  (width/2, length/2)
+
+retry_text = endfont_2.render("Retry", True, (255,255,255), (0,0,0))
+retry_rect = retry_text.get_rect()
+retry_rect.center = (width/2, length/2 + 45)
+
+home_text = endfont_2.render("Back To Home", True, (255,255,255), (0,0,0))
+home_rect = home_text.get_rect()
+home_rect.center = (width/2, length/2 + 90)
 
 while running:
 
-    font = pygame.font.Font('freesansbold.ttf', 18)
-    menufont = pygame.font.Font('freesansbold.ttf', 40)
+    mouse_x, mouse_y = pygame.mouse.get_pos()
 
+    second = int((pygame.time.get_ticks() - start_time) / 1000)
+    time_text = font.render("Time left: " + str(game_time - second), True, (0,255,0), (0,0,255))
+    timeRect = time_text.get_rect()
+    timeRect.center = (720,20)
 
-
-
-    score_text = font.render('Score: ' + str(score), True, (0, 255, 0), (0,0,255))
     hscore_text = font.render('High Score: ' + str(high_score), True, (0, 255, 0), (0,0,255))
-
-    textRect = score_text.get_rect()
     textRect2 = hscore_text.get_rect()
-
-
-    textRect.center = (52,20)
     textRect2.center = (80,40)
 
+    score_text = font.render('Score: ' + str(score), True, (0, 255, 0), (0,0,255))
+    textRect = score_text.get_rect()
+    textRect.center = (52,20)
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    screen.fill("black")
-    screen.blit(pygame.transform.scale(background.image, (width,length)), background.rect)
+    score_end_text = endfont_2.render("Score: " + str(score), True, (255,255,255), (0,0,0))
+    score_end_rect = score_end_text.get_rect()
+    score_end_rect.center = (width/2, length/2 - 40)
 
-    if drawings == True:
-        menu_text = menufont.render('START', True, (255,255,255), (0,0,0))
-        menuRect = menu_text.get_rect()
-        menuRect.center =  (width/2, length/2)
-        screen.blit(menu_text, menuRect)
+    hscore_end_text = endfont_2.render("High Score: " + str(high_score), True, (255,255,255), (0,0,0))
+    hscore_end_rect = hscore_end_text.get_rect()
+    hscore_end_rect.center = (width/2, length/2)
 
-    else:
+    if not_starting == True and Isitend == False:
+        screen.fill("white")
+        screen.blit(pygame.transform.scale(background.image, (width,length)), background.rect)
+        screen.blit(start_text, startRect)
+        screen.blit(name_text,name_rect)
+
+    elif not_starting == False and Isitend == False:
+        screen.fill("white")
+        screen.blit(pygame.transform.scale(background2.image, (width,length)), background2.rect)
         screen.blit(score_text, textRect)
         screen.blit(hscore_text,textRect2)
+        screen.blit(time_text, timeRect)
         pygame.draw.circle(screen, (255,0,0), (cir_x,cir_y), 35)
+        if second > game_time:
+            Isitend = True
+
+    else:
+        screen.fill("white")
+        screen.blit(pygame.transform.scale(background.image, (width,length)), background.rect)
+        screen.blit(gameover_text,gameover_rect)
+        screen.blit(hscore_end_text,hscore_end_rect)
+        screen.blit(score_end_text,score_end_rect)
+        screen.blit(retry_text,retry_rect)
+        screen.blit(home_text,home_rect)
+
 
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                if menuRect.collidepoint((mouse_x,mouse_y)) and drawings == True:
-                    drawings = False
-                    cir_x = random.randint(40, width-40)
-                    cir_y = random.randint(40, length-40)
-                elif (mouse_x >= (cir_x - 35) and mouse_x <= (cir_x + 35)) and (mouse_y >= (cir_y - 35) and mouse_y <= (cir_y + 35)):
-                    cir_x = random.randint(40,width-40)
-                    cir_y = random.randint(40,length-40)
+                if startRect.collidepoint((mouse_x,mouse_y)) and not_starting == True:
+                    not_starting = False
+                    start_time = pygame.time.get_ticks()
+                    score = 0
+                elif (mouse_x >= (cir_x - 35) and mouse_x <= (cir_x + 35)) and (mouse_y >= (cir_y - 35) and mouse_y <= (cir_y + 35)) and not_starting == False and Isitend == False:
                     score += 1
-                else:
+                elif not_starting == False and Isitend == False:
                     if score > 0:
                         score -= 1
-                    cir_x = random.randint(40,width-40)
-                    cir_y = random.randint(40,length-40)
+
+                if retry_rect.collidepoint((mouse_x,mouse_y)) and Isitend == True:
+                    not_starting = False
+                    Isitend = False
+                    score = 0
+                    start_time = pygame.time.get_ticks()
+                elif home_rect.collidepoint((mouse_x,mouse_y)) and Isitend == True:
+                    not_starting = True
+                    Isitend = False
+
+                cir_x = random.randint(40,width-40)
+                cir_y = random.randint(40,length-40)
 
         if event.type == pygame.QUIT:
             running = False
